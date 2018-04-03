a94fd0b81b5ec67b4bda741deb1fb1de5faae507
joaobandarra <joaogsbandarra@gmail.com>
2018-04-03 13:02:10 +0100

    Adição de restrições ao modelo + agentes + criação do interface (controller ) dos agentes

---
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Multas.Models;
+
+namespace Multas.Controllers
+{
+    public class AgentesController : Controller
+    {//Cria uma variavel que representa a base de dados
+        private MultasDb db = new MultasDb();
+
+        // GET: Agentes
+        public ActionResult Index()
+        {//db.Agentes.ToList() -> em SQL: SELECT * FROM Agentes;
+            //enviar para a view uma lista com todos os agentes, da base de dados
+            return View(db.Agentes.ToList());
+        }
+
+        // GET: Agentes/Details/5
+        public ActionResult Details(int? id)
+        {
+            //se se escrever 'int?' é possivel não forncer o valor para o ID e não há erro
+
+            //proteção para o caso de não ter sido fornecido um ID válido
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //procura na bd, o agente cujo ID foi fornecido
+            Agentes agentes = db.Agentes.Find(id);
+            //proteão para o casod e não ter sido encontrado um agente que tenha o id fornecido
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            //entrega à view os dados do agente encontrado
+            return View(agentes);
+        }
+
+        // GET: Agentes/Create
+        public ActionResult Create()
+        {
+            //apresenta a view para se inserir um novo agente
+            return View();
+        }
+
+
+
+        // POST: Agentes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //anotador para http post
+        [HttpPost]
+        //anotador para proteção por roubo de identidade
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nome,Fotografia,Esquadra")] Agentes agentes)
+        {
+            //escrever os dados de um novo Agente na BD
+
+            //ModelState.IsValid -> confronta os dados fornecidos da view com as exigências do modelo
+            if (ModelState.IsValid)
+            {
+                //adiciona o novo agente à BD
+                db.Agentes.Add(agentes);
+                //faz commit às alterações
+                db.SaveChanges();
+                //se tudo correr bem, retorna para a página de index do agente
+                return RedirectToAction("Index");
+            }
+
+            //se houver um erro, reapresenta os dados do agente
+            return View(agentes);
+        }
+
+        // GET: Agentes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agentes);
+        }
+
+        // POST: Agentes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nome,Fotografia,Esquadra")] Agentes agentes)
+        {
+            if (ModelState.IsValid)
+            {
+                //neste caso já existe um agente apenas quero editar os seus dados
+                db.Entry(agentes).State = EntityState.Modified;
+                //efetuar commit
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agentes);
+        }
+
+        // GET: Agentes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agentes agentes = db.Agentes.Find(id);
+            if (agentes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agentes);
+        }
+
+        // POST: Agentes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agentes agentes = db.Agentes.Find(id);
+            //delete do agente na bd
+            db.Agentes.Remove(agentes);
+            //commit
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -14,8 +14,16 @@ namespace Multas.Models
 
         [Key]
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [RegularExpression ("[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüãõç]+(( |'|-| dos | da | de | d'| e )[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüãõç]+){1,3}", ErrorMessage ="O {0} apenas pode conter letras e espaços em branco. cada palavra começa em maiúscula seguida de minúsculas")]
         public string Nome { get; set; }
+
+        
         public  string Fotografia { get; set; }
+
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!")]
+        [RegularExpression ("[A-ZÍÉÂÁ]*[a-záéíóúàèìòùâêîôûäëïöüãõç -]*", ErrorMessage ="Não aceita numeros")]
         public string Esquadra { get; set; }
 
 
